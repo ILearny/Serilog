7a4487da8594ff60ee10335a1a8d1eeebd2160ec
Kota <sandeshjkota@gmail.com>
2018-09-22 01:32:50 +0530

    Add files via upload

---
+++ Logger/SerilogAdapter.cs
@@ -0,0 +1,39 @@
+﻿using Serilog;
+using System;
+
+namespace Logger
+{
+    public class SerilogAdapter
+    {
+        private static ILogger _logger;
+        private static string template = "{Timestamp:yyyy-MMM-dd HH:mm::ss} [{Level}] {Message}{NewLine}{Exception}";
+
+
+        static SerilogAdapter()
+        {
+            _logger = new LoggerConfiguration()
+                            .WriteTo.Console()
+                            .WriteTo.ColoredConsole(Serilog.Events.LogEventLevel.Verbose, outputTemplate: template)
+                            .WriteTo.File("log.txt")
+                            .WriteTo.RollingFile("rollinglogfile.txt", retainedFileCountLimit: 2)
+                            .CreateLogger();
+
+            Log.Logger = _logger;
+        }
+
+        private static ILogger Logger {
+            get { return Log.Logger; }
+            set { }
+        }
+
+        public void LogMessage(string message)
+        {
+            Logger.Debug(message);
+            Logger.Information(message);
+            Logger.Warning(message);
+            Logger.Error(message);
+            Logger.Fatal(message);
+        }
+
+    }
+}
---
+++ Tester/Program.cs
@@ -0,0 +1,18 @@
+﻿using Logger;
+using System;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var logger = new SerilogAdapter();
+            logger.LogMessage("Logging message");
+
+            Console.ReadLine();
+        }
+    }
+}
